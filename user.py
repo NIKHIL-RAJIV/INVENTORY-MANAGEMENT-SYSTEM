4674f79be71079f61208043b5a871258bfa4cb50
Adithya S Prabhu <69096963+TectonicSteak@users.noreply.github.com>
2020-10-29 09:17:14 +0530

    Add files via upload

--- user.py
+++ user.py
@@ -1,94 +1,98 @@
+from frame import *
+from product import *
+from employee import *
+from  global_admin import *
 from tkinter import ttk
 from tkinter import *
 import mysql.connector
-from PIL import ImageTk,Image
 import tkinter.messagebox as tm
+from global_employee import *
+from gui_backend import *
+from admin_backend import *
+from developer import *
+
+username = 'root'
+password = 'chalakudy'
+con = mysql.connector.connect(host='localhost',database="tablemaker",user=username,password=password)
+cur =con.cursor()
 
 
+
+def check_admin(username,password):
+	cur.execute("select Ad_name,Password from ADMIN")
+	for i,j in cur.fetchall():
+		if username==i.lower() and password==j.lower():
+			return True
+	return False
+
+
+def check_employee(username,password):
+	cur.execute("select E_name,Password from EMP")
+	for i,j in cur.fetchall():
+		if username==i.lower() and password==j.lower():
+			return True
+	return False
 
 def login():
+	fgc, bgc=("white","#182E52")
+	#bgc = '#212B35'
+	font_16 = ("black",16)
+	font_18 = ("black",18)
+
 	root=Tk()
-	username = 'root'
-	password = '#MORUS'
-	con = mysql.connector.connect(host='localhost',database="tablemaker",user=username,password=password)
-	cur =con.cursor()
-	root.resizable(0,0)
 	root.geometry("400x650")
-	root.config(bg="#182E52")
-	fgc, bgc=("black","white")
+	root.config(bg=bgc)
 	root.title("Login")
-	
-	'''
-	image2 =Image.open("blue.png")
-	image1 = ImageTk.PhotoImage(image2)
-	w = image1.width()
-	h = image1.height()
-	root.geometry('%dx%d+0+0' % (w,h))
-	
-	
-	canvas=Canvas(root,width=1920,height=1080)
-    image=ImageTk.PhotoImage(Image.open("blue.png"))
-    canvas.create_image(0,0,anchor=tkinter.NW,image=image)
-    canvas.place()
-    '''
-	
-	#my_img=ImageTk.PhotoImage(Image.Open("icon.png"))
-	#my_Label=Label(image=my_img)
 
+	oldemp1 = Image.open('assets/user.png')
+	newemp1 = oldemp1.resize((120,120))
+	emp_pic = ImageTk.PhotoImage(newemp1)
+	E_Button = Label(root,image=emp_pic,bd=0).place(x=120,y=20)
 
-	#my_Label.place(x=80,y=10)
-	variable = StringVar(root)
-	variable.set("  Login As ")
-	values=("ADMIN","EMPLOYEE")
-	w=ttk.Combobox(root,value=values)
-	w.place(x=110,y=190)
+	values=("ADMIN","EMPLOYEE","DEVELOPER")
+	role=ttk.Combobox(root,value=values,font=("black",12))
 
-	name=Entry(root,width=15,fg=fgc,bg=bgc,font=("black",18))
-	password=Entry(root,width=15,fg=fgc,bg=bgc,font=("black",18), show="*")
-	Label_name=Label(root,text="Username  ",fg=fgc,bg=bgc,font=("black",16))
-	Label_password=Label(root,text="Password  ",fg=fgc,bg=bgc,font=("black",16))
+	name=Entry(root,width=15,fg=fgc,bg=bgc,font=font_18)
+	password=Entry(root,width=15,fg=fgc,bg=bgc,font=font_18, show="*")
+	Label_name=Label(root,text="Username",fg=fgc,bg=bgc,font=font_16)
+	Label_password=Label(root,text="Password",fg=fgc,bg=bgc,font=font_16)
+
 	def call():
-		if w.get()=="ADMIN":
-			cur.execute("select Ad_name,Password from ADMIN")		
-			Username=name.get()
-			Password=password.get()
-
-			for i,j in cur.fetchall():
-				if Username==i and Password==j:
-					tm.showinfo("login info","welcome man")
-					break
+		Username=name.get().lower()
+		Password=password.get().lower()
+		
+		if role.get()=="ADMIN":
+			if check_admin(Username,Password):
+				admin()
 			else:
-				tm.showerror("login error","Incorrect username or password")
-	
+				tm.showerror("login error","invalid username or password")
 
-			#name=name.get()
-			#password=password.get()
-		 
-		elif w.get()=="EMPLOYEE":
-			cur.execute("select E_name,Password from EMP")
-			
-			Username=name.get()
-			Password=password.get()
-			for i,j in cur.fetchall():
-				if Username==i and Password==j:
-					tm.showinfo("login info","welcome mahn")
-					break
-
+		elif role.get()=="EMPLOYEE":
+			if check_employee(Username,Password):
+				billing_area()
 			else:
-				tm.showerror("login error","Incorrect username1 or password")
-					
+				tm.showerror("login error","invalid username or password")
+
+		elif role.get()=="DEVELOPER":
+			if check_admin(Username,Password):
+				window()
+			else:
+				tm.showerror("login error","invalid username or password")
+
 		else:
 			tm.showerror("Error","Choose option")
-	con.commit()
-	btn_login = Button(root,text="login",fg=fgc,bg=bgc,font=("blue",16),bd=2,command=call)
-	btn_login.place(x=140,y=315)
 
-	Label_name.place(x=0,y=235)
-	name.place(x=120,y=235)
-	Label_password.place(x=0,y=275)
-	password.place(x=120,y=275)
+	btn_login = Button(root,text="login",fg=fgc,bg=bgc,font=font_16,bd=2,command=call)
+
+	name.place(x=140,y=235)
+	role.place(x=140,y=190)
+	password.place(x=140,y=275)
+	btn_login.place(x=280,y=315)
+	Label_name.place(x=20,y=235)
+	Label_password.place(x=20,y=275)
+
 	root.mainloop()
 
-	
+
 
 login()
